dfcba5b96a1792a087123b98257b7cbcb5c10a2b
João Pedro de Almeida <joaopedro.alm.c@gmail.com>
2021-12-03 01:02:51 -0300

    terminei

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,15 +1,41 @@
 document.body.onload = function () {
-	const h = document.getElementsByTagName('h3');
-// Vai mostrar na tag h3 as frases abaixo
+	// Vai mostrar na tag h3 as frases abaixo
 	function writeInH3() {
+		const h3 = document.getElementsByTagName('h3');
 		const obj = [
-			'sons das entradas',
-			'sons dos personagens',
-			'sons de algumas ações',
+			'abertura até a parte da bruxa',
+			'os bixos vão começar a entrar',
+			'terminando a peça',
 		];
 		for (let i = 0; i < obj.length; i++) {
-			h[i].innerText = obj[i].toUpperCase();
+			h3[i].innerText = obj[i].toUpperCase();
 		}
 	}
 	writeInH3();
+
+	// Vai mostrar na tag h4 as frases abaixo
+	function writeInH4() {
+		const h4 = document.getElementsByTagName('h4');
+		const obj = [
+			'jardim',
+			'música alegre',
+			'suspense da bruxa',
+			'preguiça',
+			'pantera rosa',
+			'rato',
+			'raposa',
+			'leão',
+			'fogo',
+			'ventania',
+			'música triste',
+			'aleluia',
+			'corre bruxa',
+			'pantera rosa',
+			'música de encerramento',
+		];
+		for (let i = 0; i < obj.length; i++) {
+			h4[i].innerText = `${i + 1} - ${obj[i].toUpperCase()}`;
+		}
+	}
+	writeInH4();
 };
